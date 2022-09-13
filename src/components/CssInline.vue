73e070b4ad16d6b72d26e58d86ce4b9c4c38694e
Fílip Anselmo Alves da Silva <filip.anselmo@jointecnologia.com.br>
2022-09-13 09:30:48 -0400

    Alterando o CssInline

--- src/components/CssInline.vue
+++ src/components/CssInline.vue
@@ -6,6 +6,11 @@
         <p style="color: red;">
             Isso é um paragráfo
         </p>
+        <div>
+            <h3 style="background: yellow; color: blue;">
+                Eu sou o Batman;
+            </h3>
+        </div>
     </div>
 </template>
 
