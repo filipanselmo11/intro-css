ceb7eddf2072b3b236ff5404aff2334cea1d81b1
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2022-09-13 10:18:54 -0400

    Cores RGB

---
+++ src/components/CoresRgb.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>
+        Cores específicas usando valores RGB
+    </h1>
+    <div>
+        <h2 style="background-color: rgb(255, 0, 0);">
+            rgb(255, 0, 0);
+        </h2>
+        <h2 style="background-color: rgb(0, 0, 255);">
+            rgb(0, 0, 255);
+        </h2>
+        <h2 style="bakground-color: rgb(60, 179, 113);">
+            rgb(60, 179, 113);
+        </h2>
+        <h2 style="background-color: rgb(238, 130, 238);">
+            rgb(238, 130, 238);
+        </h2>
+        <h2 style="background-color: rgb(255, 165, 0);">
+            rgb(255, 165, 0);
+        </h2>
+        <h2 style="background-color: rgb(106, 90, 205);">
+            rgb(106, 90, 205);
+        </h2>
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
