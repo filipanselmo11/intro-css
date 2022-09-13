5411b31a78050693dd19047944d588119bdbd82f
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2022-09-13 13:23:38 -0400

    Anexo de fundo

---
+++ src/components/anexo-de-fundo/AbreviacaoFundo.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <h1>The background Property</h1>
+
+    <p>
+      The background property is a shorthand property for specifying all the
+      background properties in one declaration.
+    </p>
+
+    <p>
+      Here, the background image is only shown once, and it is also positioned
+      in the top-right corner.
+    </p>
+
+    <p>
+      We have also added a right margin, so that the text will not write over
+      the background image.
+    </p>
+    <div>
+        <h1>Todas as propriedades de plano de fundo Css</h1>
+        <div>
+            <h1>background</h1>
+            <p>
+                Define todas as propriedades do plano de fundo em uma declaração
+            </p>
+        </div>
+        <div>
+            <h1>background-attachment</h1>
+            <p>
+                Define se uma imagem de fundo é fixa ou rola com o resto da página
+            </p>
+        </div>
+        <div>
+            <h1>background-clip</h1>
+            <p>
+                Especifica a área de pintura do plano de fundo
+            </p>
+        </div>
+        <div>
+            <h1>background-color</h1>
+            <p>
+                Define a cor de fundo de um elemento
+            </p>
+        </div>
+        <div>
+            <h1>background-image</h1>
+            <p>
+                Define a imagem de fundo para um elemento
+            </p>
+        </div>
+        <div>
+            <h1>background-origin</h1>
+            <p>
+                Especifica onde a(s) imagem(ns) de fundo está(ão) posicionada(s)
+            </p>
+        </div>
+        <div>
+            <h1>background-position</h1>
+            <p>
+                Define a posição inicial de uma imagem de fundo
+            </p>
+        </div>
+        <div>
+            <h1>background-repeat</h1>
+            <p>
+                Define como uma imagem de fundo será repetida
+            </p>
+        </div>
+        <div>
+            <h1>background-size</h1>
+            <p>
+                Especifica o tamanho das imagens de fundo
+            </p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AbreviacaoFundo'
+};
+</script>
+
+<style scoped>
+div {
+    background: #FFFFFF url('img_tree.png') no-repeat right top; /**Propriedade abreviada */
+    margin-right: 200px;
+}
+</style>
---
+++ src/components/anexo-de-fundo/SegundoPlanoCss.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>The background-attachment Property</h1>
+
+    <p>
+      The background-attachment property specifies whether the background image
+      should scroll or be fixed (will not scroll with the rest of the page).
+    </p>
+
+    <p>
+      <strong>Tip:</strong> If you do not see any scrollbars, try to resize the
+      browser window.
+    </p>
+
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+    <p>The background-image is fixed. Try to scroll down the page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SegundoPlanoCss',
+};
+</script>
+
+<style scoped>
+div {
+    background-image: url('img_tree.png');
+    background-repeat: no-repeat;
+    background-position: right top;
+    margin-right: 200px;
+    /* background-attachment: fixed; */
+    background-attachment: scroll;
+}
+</style>
