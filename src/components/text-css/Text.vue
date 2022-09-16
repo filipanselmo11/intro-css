f62a5ad36333cf31d9e97f0732c3fa8902cc4709
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2022-09-16 16:58:53 -0400

    Text-css

---
+++ src/components/text-css/Text.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <section id="sec1">
+      <div>
+        <h1>text formatting</h1>
+        <p>
+          This text is styled with some of the text formatting properties. The
+          heading uses the text-align, text-transform, and color properties. The
+          paragraph is indented, aligned, and the space between characters is
+          specified. The underline is removed from this colored
+          <a target="_blank" href="tryit.asp?filename=trycss_text"
+            >"Try it Yourself"</a
+          >
+          link.
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Text',
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid gray;
+  padding: 8px;
+}
+
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  color: #4CAF50;
+}
+
+p {
+  text-indent: 50px;
+  text-align: justify;
+  letter-spacing: 3px;
+}
+
+a {
+  text-decoration: none;
+  color: #008CBA;
+}
+</style>
---
+++ src/components/text-css/TextColor.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <section id="text-color">
+      <h1>This is heading 1</h1>
+      <p>
+        This is an ordinary paragraph. Notice that this text is blue. The
+        default text color for a page is defined in the body selector.
+      </p>
+      <p>Another paragraph.</p>
+    </section>
+    <section id="text-color-background">
+      <h1>This is a Heading</h1>
+      <p>This page has a grey background color and a blue text.</p>
+      <div>This is a div.</div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextColor",
+};
+</script>
+
+<style scoped>
+body {
+  color: blue;
+}
+
+h1 {
+  color: green;
+}
+
+#text-color-background {
+    background-color: lightgrey;
+    color: blue;
+}
+
+h1 {
+    background-color: black;
+    color: white;
+}
+
+div {
+    background-color: blue;
+    color: white;
+}
+</style>
